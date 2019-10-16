102327aa82c1326b8bf3366b89315c4611410291
nashirat <55182333+nashirat@users.noreply.github.com>
2019-10-16 13:42:16 +0700

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,33 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$hashpass = hash('sha256',$password);
+//dbx_connect
+  if (!empty($username)){
+if (!empty($password)){
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = new mysqli('localhost','root','','register');
+if($conn->connect_error){
+  die('Connection Failed : '.$conn->connect_error);
+}else {
+  $stmt = $conn->prepare("Insert into users(username, password, email)
+  values(?, ?, ?)");
+  $stmt->bind_param('sss',$username, $password, $email);
+  $stmt->execute();
+  echo "register success";
+  $stmt->close();
+  $conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+header('Location: /landing.html');
+ ?>
